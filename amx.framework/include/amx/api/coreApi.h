e164af362dd5afc01ea815ea5e837cb6ba615f0a
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2024-01-24 10:44:21 -0500

    merp

--- amx.framework/include/amx/api/coreApi.h
+++ amx.framework/include/amx/api/coreApi.h
@@ -8,6 +8,9 @@
 using scene_list = std::vector<std::shared_ptr<amx::Scene>>;
 
 namespace amx {
+    /// @brief Create an instance of a game
+    /// @param config Initialization config for the game
+    /// @return Reference to the created game
     inline std::shared_ptr<Game> createGameInstance(GameInitConfig config) {
         auto game = std::make_shared<Game>(config);
 
